1c6828f77dd13c18af2f3fa3b8189550a60cc406
Ali Khan <60252563+Ali-here@users.noreply.github.com>
2026-07-13 15:26:01 +0500

    updated

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -4,7 +4,7 @@ import { SiteNav } from "@/components/site/SiteNav";
 import { SiteFooter } from "@/components/site/SiteFooter";
 import { PageHero } from "@/components/site/PageHero";
 import teamDirector from "@/assets/team-director.jpeg";
-import teamBiologist from "@/assets/team-biologist.jpg";
+import teamBiologist from "@/assets/team-biologist.jpeg";
 import teamCare from "@/assets/team-care.jpeg";
 import teamNeurologist from "@/assets/team-neurologist.jpeg";
 
